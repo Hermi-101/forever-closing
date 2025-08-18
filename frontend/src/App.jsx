4ef0f0397ed4f1f1f6305098109990968900ca49
Hermi-101 <105059748+Hermi-101@users.noreply.github.com>
2025-08-18 21:54:41 +0300

    Created the fuctionality of addtoCart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,12 +12,14 @@ import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
-
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 const App = () => {
   
   return (
     
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer/>
       
       <Navbar />
       <SearchBar/>
@@ -26,7 +28,7 @@ const App = () => {
         <Route path='/collection' element={<Collection/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
-        <Route path='/products/:productID' element={<Products/>}/>
+        <Route path='/products/:productId' element={<Products/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/placeorder' element={<PlaceOrder/>}/>
